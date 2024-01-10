d1c40cb1bd74eea12395224ab992c58e6593d469
Holylogus <hamar.patrik94@gmail.com>
2024-01-10 20:08:39 +0100

    Routok beállítása, ThunderTesztek

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index(){
+        $book = response()->json(Book::all());
+        return $book;
+    }
+    public function show($id){ 
+        $book = response()->json(Book::find($id));
+        return $book;
+    }
+
+    public function store(Request $request){
+        $book = new Book();
+        $book->title=$request->title;
+        $book->author=$request->author;
+        $book->price=$request->price;
+        $book->img=$request->img;
+        $book->category=$request->category;
+        $book->save();
+    }
+    
+    public function update($id,Request $request){
+        $book = Book::find($id);
+        $book->title=$request->title;
+        $book->author=$request->author;
+        $book->price=$request->price;
+        $book->img=$request->img;
+        $book->category=$request->category;
+        $book->save();  
+    }
+
+    public function destroy($id){
+        Book::find($id)->delete();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('books', [BookController::class, 'index']);
+Route::get('books/{id}', [BookController::class, 'show']);
+Route::post('books', [BookController::class, 'store']);
+Route::put('books/{id}', [BookController::class, 'update']);
+Route::delete('books/{id}', [BookController::class, 'destroy']);
