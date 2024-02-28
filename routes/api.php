e1cd74c874798d48b743b0fd6ece14c4f9db7a9d
Holylogus <hamar.patrik94@gmail.com>
2024-02-28 18:22:42 +0100

    Breeze api install, login hozzáadása, token hozzáadása web.php-ban

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\BookController;
-use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,18 +9,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-//Könyvek kérések
-Route::get('books', [BookController::class, 'index']);
-Route::get('books/{id}', [BookController::class, 'show']);
-Route::post('books', [BookController::class, 'store']);
-Route::put('books/{id}', [BookController::class, 'update']);
-Route::delete('books/{id}', [BookController::class, 'destroy']);
